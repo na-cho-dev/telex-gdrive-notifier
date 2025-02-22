d1a328c03c310e025d5c7778499c44d12c497ff6
NACHO <nachodev369@gmail.com>
2025-02-22 13:01:20 +0100

    Fix: Modified Telex JSON Integration

--- controllers/jsonIntegrationController.js
+++ controllers/jsonIntegrationController.js
@@ -4,26 +4,26 @@ const jsonIntegration = (req, res) => {
   // const { baseURL } = getConfig();
   const baseURL = req.protocol + "s://" + req.get("host");
 
-  const integration_json_data = {
+  const jsonIntegration = {
     "data": {
       "date": {
-        "created_at": "2025-02-19",
-        "updated_at": "2025-02-19"
+        "created_at": "2025-02-22",
+        "updated_at": "2025-02-22"
       },
       "descriptions": {
-        "app_name": "Telex GDrive Notifier",
         "app_description": "Telex GDrive Notifier is a lightweight service that monitors Google Drive and sends real-time backup notifications to a Telex Channel 🚀",
         "app_logo": "https://iili.io/2ybBmwQ.png",
+        "app_name": "Telex GDrive Notifier",
         "app_url": baseURL,
-        "background_color": "#fff"
+        "background_color": "#HEXCODE"
       },
-      "is_active": false,
       "integration_category": "Cloud Services",
       "integration_type": "interval",
+      "is_active": false,
       "key_features": [
-        "Folder Backup",
-        "File Backup",
-        "Notify File Change"
+        "File Change Update",
+        "File Deleted Update",
+        "File Modified Update"
       ],
       "permissions": {
         "monitoring_user": {
@@ -36,7 +36,7 @@ const jsonIntegration = (req, res) => {
           "label": "interval",
           "type": "text",
           "required": true,
-          "default": "*/10 * * * * *"
+          "default": "* * * * *"
         },
         {
           "label": "Folder ID",
@@ -45,12 +45,58 @@ const jsonIntegration = (req, res) => {
           "default": "103QnFCkx6PIv25Pxt3rE1uRkNun0ZKiT"
         }
       ],
-      "target_url": "",
-      "tick_url": `${baseURL}/tick`
+      "tick_url": `${baseURL}/tick`,
+      "target_url": ""
     }
   }
 
-  res.status(200).json(integration_json_data)
+  // const integration_json_data = {
+  //   "data": {
+  //     "date": {
+  //       "created_at": "2025-02-19",
+  //       "updated_at": "2025-02-19"
+  //     },
+  //     "descriptions": {
+  //       "app_name": "Telex GDrive Notifier",
+  //       "app_description": "Telex GDrive Notifier is a lightweight service that monitors Google Drive and sends real-time backup notifications to a Telex Channel 🚀",
+  //       "app_logo": "https://iili.io/2ybBmwQ.png",
+  //       "app_url": baseURL,
+  //       "background_color": "#fff"
+  //     },
+  //     "is_active": false,
+  //     "integration_category": "Cloud Services",
+  //     "integration_type": "interval",
+  //     "key_features": [
+  //       "Folder Backup",
+  //       "File Backup",
+  //       "Notify File Change"
+  //     ],
+  //     "permissions": {
+  //       "monitoring_user": {
+  //         "always_online": true,
+  //         "display_name": "Telex GDrive Notifier"
+  //       }
+  //     },
+  //     "settings": [
+  //       {
+  //         "label": "interval",
+  //         "type": "text",
+  //         "required": true,
+  //         "default": "*/10 * * * * *"
+  //       },
+  //       {
+  //         "label": "Folder ID",
+  //         "type": "text",
+  //         "required": true,
+  //         "default": "103QnFCkx6PIv25Pxt3rE1uRkNun0ZKiT"
+  //       }
+  //     ],
+  //     "target_url": "",
+  //     "tick_url": `${baseURL}/tick`
+  //   }
+  // }
+
+  res.status(200).json(jsonIntegration)
 }
 
 export default jsonIntegration;
